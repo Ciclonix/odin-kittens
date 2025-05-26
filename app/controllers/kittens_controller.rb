45deb72d424c0235f6d472d77982f8f4773b3ce8
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2025-05-26 15:56:16 +0200

    Create Kitten model and controller

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,52 @@
+class KittensController < ApplicationController
+  before_action :set_kitten, only: %i[ show edit update destroy ]
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:notice] = "Created kitten"
+      redirect_to @kitten
+    else
+      flash.now[:alert] = "Could not create kitten"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @kitten.save
+      flash[:notice] = "Edited kitten"
+      redirect_to @kitten
+    else
+      flash.now[:alert] = "Could not edit kitten"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten.destroy!
+    redirect_to kittens_path
+  end
+
+  private
+    def set_kitten
+      @kitten = Kitten.find(params[:id])
+    end
+
+    def kitten_params
+      params.expect(kitten: %i[ name age cuteness softness ])
+    end
+end
